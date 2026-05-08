dbee0a3e4f838603ca0af77ab4be7facfe444a7f
Zach Zimmerman <zpzimmerman@gmail.com>
2026-05-08 09:10:16 -0700

    test: bump source to test.pypi pyscamp-4.0.2.dev2 sdist
    The upstream PR #137 has been merged and the dev2 sdist has been
    uploaded to test.pypi.org via the new OIDC trusted-publishing workflow.
    Switch from a git_url+git_rev source to a proper sdist+sha256.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@ export SCAMP_USE_EXTERNAL_EIGEN=ON
 export PYSCAMP_USE_EXTERNAL_PYBIND11=ON
 export SCAMP_ENABLE_BINARY_DISTRIBUTION=ON
 # Pin version so setuptools_scm does not produce a local-version string
-export SETUPTOOLS_SCM_PRETEND_VERSION=4.0.2dev0
+export SETUPTOOLS_SCM_PRETEND_VERSION=4.0.2.dev2
 ${PYTHON} setup.py install
