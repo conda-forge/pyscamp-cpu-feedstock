5d4a7faafc5c15db43da5e521023c1e7c53c1f62
Zach Zimmerman <zpzimmerman@gmail.com>
2026-05-08 19:02:15 -0700

    Update to pyscamp 4.0.2 release

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,4 @@ export VERBOSE=ON
 export SCAMP_USE_EXTERNAL_EIGEN=ON
 export PYSCAMP_USE_EXTERNAL_PYBIND11=ON
 export SCAMP_ENABLE_BINARY_DISTRIBUTION=ON
-# Pin version so setuptools_scm does not produce a local-version string
-export SETUPTOOLS_SCM_PRETEND_VERSION=4.0.2.dev2
 ${PYTHON} setup.py install
